12bb158e333bfd60c3ebd93573c92fc45c0baac4
Nick Liffen <UP653591@myport.ac.uk>
2016-04-18 18:00:49 +0100

    Sorting Database problems with heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 "use strict";
 import mysql from 'mysql';
-
+/*
 //Insert Credentials to Connect to our database
 const connection = mysql.createConnection({
   host     : process.env.DB_HOST,
@@ -20,6 +20,40 @@ connection.connect(function(err) {
   console.log('Connected to Database');
   console.log('Connected to Database using the following ID:  ' + connection.threadId);
 });
-
+*/
 //Making the connection avaiible to the application.
+
+let db_config = {
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASS,
+  database : process.env.DB_DATABASE,
+  port     : process.env.DB_PORT,
+  multipleStatements: true
+};
+
+let connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 module.exports = connection;
