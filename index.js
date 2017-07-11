4115da63d237898582a31db89ebf5321824c133c
Nick Liffen <NickLiffen@users.noreply.github.com>
2017-07-11 21:57:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 //Require Babel for my ES6 to ES5 translation
 require("babel-core/register");
-//Require the Server File for Babel to use, this is the first file called.
+//Require the Server File for Babel to use, this is the first file called. Added
 require("./server.js");
